7ec7dccbdad8f366b9e288791bfd8b6c7a4213e8
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2021-03-09 14:40:59 +0000

    Added tests for coordinate system: - cells are indexed using numpy array indexing order (row col). - agents have x, y and angle properties (can only be get)

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,6 @@
+import math
+import pytest
+
 from jungle import Agent, EmptyJungle
 from jungle.utils import Definitions, Actions
 
@@ -8,6 +11,7 @@ def test_rl_loop():
     agent_2 = Agent(range=6)
 
     simple_jungle = EmptyJungle(size=11)
+    assert simple_jungle.size == 11
 
     simple_jungle.add_agents(agent_1, agent_2)
 
@@ -30,6 +34,71 @@ def test_rl_loop():
     assert not done[agent_1] and not done[agent_2]
 
 
+def check_corners(envir):
+
+    # Verify that all corners have the same shape
+    # cells are identified using np coordinates
+
+    # Top-left corner
+    assert envir.cell_type(0, 0) == Definitions.OBSTACLE
+    assert envir.cell_type(0, 1) == Definitions.OBSTACLE
+    assert envir.cell_type(0, 2) == Definitions.OBSTACLE
+    assert envir.cell_type(1, 0) == Definitions.OBSTACLE
+    assert envir.cell_type(2, 1) == Definitions.OBSTACLE
+    assert envir.cell_type(1, 1) == Definitions.EXIT
+
+    # Top-right corner
+    assert envir.cell_type(0, envir.size) == Definitions.OBSTACLE
+    ...
+    #TODO: write for each corner
+
+
+def test_environment_building():
+
+    for size_envir in range(1, 20):
+
+        # Check that pair size_envir raise error
+        # or that too small environment raise error
+
+        if size_envir % 2 == 0 or size_envir < Definitions.MIN_SIZE_ENVIR:
+
+            with pytest.raises(Exception):
+                simple_jungle = EmptyJungle(size=size_envir)
+
+        else:
+
+            simple_jungle = EmptyJungle(size=size_envir)
+            check_corners(simple_jungle)
+
+
+def test_initialization():
+
+    agent_1 = Agent(range=4)
+    agent_2 = Agent(range=4)
+
+    for size_envir in [11, 13, 15]:
+
+        simple_jungle = EmptyJungle(size=size_envir)
+        simple_jungle.add_agents(agent_1, agent_2)
+
+        # grid_position should be in np coordinates (row, col)
+        assert agent_1.grid_position == ((size_envir-1)/2, (size_envir-1)/2 - 1)
+        assert agent_2.grid_position == ((size_envir-1)/2, (size_envir-1)/2 + 1)
+
+        # angle is index of trigonometric angle (0, 1, ... to 5)
+        assert agent_1.angle == 3
+        assert agent_2.angle == 0
+
+        # Cartesian coordinates have unit 1.
+
+        # on middle line, indented so +0.5
+        assert agent_1.x == agent_1.grid_position[1] + 0.5
+        assert agent_1.y == (size_envir-1 - agent_1.grid_position[0]) * math.sqrt(3) / 2
+
+        assert agent_2.x == agent_2.grid_position[1] + 0.5
+        assert agent_2.y == (size_envir-1 - agent_2.grid_position[0]) * math.sqrt(3) / 2
+
+
 def test_movements():
 
     agent_1 = Agent(range=4)
@@ -39,28 +108,30 @@ def test_movements():
 
     simple_jungle.add_agents(agent_1, agent_2)
 
-    # position should be in cartesian coordinates
-    assert agent_1.position == (4, 5, 3)
-    assert agent_2.position == (6, 5, 0)
+    # grid_position should be in np coordinates (row, col, angle)
+    assert agent_1.grid_position == (5, 4)
+    assert agent_1.angle == 3
 
-    # First rotation, then forward
-    actions = {agent_1: {Actions.FORWARD: 1, Actions.ROTATE: -1},
-               agent_2: {Actions.FORWARD: 1, Actions.ROTATE: 1}
+    assert agent_2.grid_position == (5, 6)
+    assert agent_2.angle == 0
+
+    # First rotation, then forward, but the order in the actions dict doesn't matter.
+    actions = {agent_1: {Actions.FORWARD: 1, Actions.ROTATE: 1},
+               agent_2: {Actions.FORWARD: 1, Actions.ROTATE: -1}
                }
 
     obs, rew, done = simple_jungle.step(actions)
-    assert agent_1.position == (4, 6, 2)
-    assert agent_2.position == (7, 4, 5)
 
-    # Check that 6 rotations bring to the original position
-    actions = {agent_1: {Actions.ROTATE: -1},
-               agent_2: {Actions.ROTATE: 1}
-               }
+    # Check new positions on grid
+    assert agent_1.grid_position == (6, 4)
+    assert agent_1.angle == 4
 
-    for _ in range(6):
-        obs, rew, done = simple_jungle.step(actions)
+    assert agent_2.grid_position == (4, 7)
+    assert agent_2.angle == 5
 
-    assert agent_1.position == (4, 6, 2)
-    assert agent_2.position == (7, 4, 5)
+    # Check new cartesian coordinates
+    assert agent_1.x == agent_1.grid_position[1]
+    assert agent_1.y == ((simple_jungle.size-1) - agent_1.grid_position[0]) * math.sqrt(3) / 2
 
-
+    assert agent_2.x == agent_2.grid_position[1]
+    assert agent_2.y == ((simple_jungle.size - 1) - agent_2.grid_position[0]) * math.sqrt(3) / 2

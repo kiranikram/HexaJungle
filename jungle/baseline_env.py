28c3dbf0dc76d83015d176d70f76e068294229a5
Kiran Ikram <kiran.ikram91@gmail.com>
2021-03-09 20:55:13 +0500

    generated env with exits

--- jungle/baseline_env.py
+++ jungle/baseline_env.py
@@ -4,6 +4,67 @@ import random
 
 # sets general layout of grid in hexagonal format
 
+def boundaries(local_exit, exit_type, grid):
+    r = local_exit[0]
+    c = local_exit[1]
+    if local_exit[0] == 0:
+
+        grid[r + 1, c - 1] = exit_type
+        grid[r + 2, c] = exit_type
+        grid[r + 1, c + 1] = exit_type
+
+    elif local_exit[1] == 10:
+
+        grid[r - 2, c] = exit_type
+        grid[r - 1, c - 1] = exit_type
+        grid[r + 1, c - 1] = exit_type
+        grid[r + 2, c] = exit_type
+
+    elif local_exit[1] == 0:
+
+        grid[r - 2, c] = exit_type
+        grid[r - 1, c + 1] = exit_type
+        grid[r + 1, c + 1] = exit_type
+        grid[r + 2, c] = exit_type
+
+    return grid
+
+
+def set_centroids(grid, exits):
+    for r in range(0, grid.shape[0], 5):
+        for c in range(1, grid.shape[1], 2):
+            grid[r, c] = 22
+            if r == 0:
+                exits.append([r, c])
+
+    for r in range(3, grid.shape[0], 5):
+        for c in range(0, grid.shape[1] + 1, 2):
+            grid[r, c] = 44
+            if c == grid.shape[1]:
+                exits.append([r, c])
+            elif c == 0:
+                exits.append([r, c])
+
+    return exits
+
+
+def set_exits(grid, exits):
+    open_exit = random.sample(exits, 1)
+
+    exits.remove(open_exit[0])
+
+    river_exit = random.sample(exits, 1)
+
+    exits.remove(river_exit[0])
+    grid = boundaries(river_exit[0], 5, grid)
+
+    boulder_exit = random.sample(exits, 1)
+
+    exits.remove(boulder_exit[0])
+    grid = boundaries(boulder_exit[0], 7, grid)
+
+    return grid
+
 
 class JungleGrid:
 
@@ -14,66 +75,13 @@ class JungleGrid:
 
         self.env = np.zeros((self.height, self.width), dtype=int)
 
-    def set_centroids(self):
-        grid = self.env
+        self.exits = set_centroids(self.env, self.exits)
 
-        for r in range(1, grid.shape[0], 5):
-            for c in range(1, grid.shape[1], 2):
-                # grid[r, c] = 22
-                if r == 0:
-                    self.exits.append([r, c])
+        self.env = set_exits(self.env, self.exits)
 
-        for r in range(3, grid.shape[0], 5):
-            for c in range(0, grid.shape[1] + 1, 2):
-                # grid[r,c] = 44
-                if c == grid.shape[1]:
-                    self.exits.append([r, c])
-                elif c == 0:
-                    self.exits.append([r, c])
-
-    def set_exits(self, grid):
-
-        def boundaries(local_exit, exit_type, grid):
-            r = exit[0]
-            c = exit[1]
-            if exit[0] == 0:
-
-                grid[r + 1, c - 1] = exit_type
-                grid[r + 2, c] = exit_type
-                grid[r + 1, c + 1] = exit_type
-
-            elif exit[1] == 10:
-
-                grid[r - 2, c] = exit_type
-                grid[r - 1, c - 1] = exit_type
-                grid[r + 1, c - 1] = exit_type
-                grid[r + 2, c] = exit_type
-
-            elif exit[1] == 0:
-
-                grid[r - 2, c] = exit_type
-                grid[r - 1, c + 1] = exit_type
-                grid[r + 1, c + 1] = exit_type
-                grid[r + 2, c] = exit_type
-
-            return grid
-
-        open_exit = random.sample(self.exits, 1)
-
-        self.exits.remove(open_exit[0])
-
-        river_exit = random.sample(self.exits, 1)
-
-        self.exits.remove(river_exit[0])
-        grid = boundaries(river_exit[0], 5, grid)
-
-        boulder_exit = random.sample(self.exits, 1)
-
-        self.exits.remove(boulder_exit[0])
-        grid = boundaries(boulder_exit[0], 7, grid)
-
-        return grid
+    def env(self):
+        return self.env
 
 
-jungle = JungleGrid(10, 10)
+jungle = JungleGrid(11, 11)
 print(jungle.env)

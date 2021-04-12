acd980ca3062ffb7f7dd177a8bcd828660717cbc
mgarciaortiz <michael.garcia.ortiz@gmail.com>
2021-04-12 10:12:55 +0100

    Added simple test environments: - easy exit - river exit

--- jungle/rl_envs/basic.py
+++ jungle/rl_envs/basic.py
@@ -12,3 +12,14 @@ class EasyExit(EmptyJungle):
         self.add_object(ElementsEnv.EXIT_EASY, exit_1.coordinates)
 
 
+class RiverExit(EmptyJungle):
+
+    def __init__(self, size):
+
+        super().__init__(size)
+
+        exit_1 = self.select_random_exit()
+        self.add_object(ElementsEnv.EXIT_DIFFICULT, exit_1.coordinates)
+        self.add_object(ElementsEnv.RIVER, exit_1.surrounding_1)
+        self.add_object(ElementsEnv.RIVER, exit_1.surrounding_2)
+
